81b7bd25596a3090997793f8cb34c704cf2287a4
Clif Hudson <clifrunsalot@yahoo.com>
2017-03-12 12:08:01 -0400

    Added mobile_apps/week3 files

---
+++ coursera/full_stack/mobile_apps/week3/ionic/conFusion/www/js/services.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('conFusion.services', ['ngResource'])
+    .constant("baseURL", "http://localhost:3000/")
+
+.factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+    return $resource(baseURL + "dishes/:id", null, {
+        'update': {
+            method: 'PUT'
+        }
+    });
+
+}])
+
+.factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+    return $resource(baseURL + "promotions/:id");
+    
+  }])
+
+.factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+        var corpfac = {};
+
+        corpfac.getLeaders = function () {
+            return $resource(baseURL + "leadership/:id", null, {
+                'update': {
+                    method: 'PUT'
+                }
+            });
+        };
+
+        // Remember this is a factory not a service
+        return corpfac;
+
+}])
+    .factory('feedbackFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+        return $resource(baseURL + "feedback/:id");
+
+  }])
+
+.factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    var favFac = {};
+    var favorites = [];
+
+    favFac.addToFavorites = function (index) {
+        for (var i = 0; i < favorites.length; i++) {
+            if (favorites[i].id == index)
+                return;
+        }
+        favorites.push({
+            id: index
+        });
+    };
+
+    favFac.deleteFromFavorites = function (index) {
+        for (var i = 0; i < favorites.length; i++) {
+            if (favorites[i].id == index) {
+                favorites.splice(i, 1);
+            }
+        }
+    }
+
+    favFac.getFavorites = function () {
+        return favorites;
+    };
+
+    return favFac;
+}])
+
+;
